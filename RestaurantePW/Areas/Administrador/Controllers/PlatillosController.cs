30fe283c38b639e6a012c88357d389c701f28a09
Rodrigo <alejandroguzmancruz@outlook.com>
2023-01-04 03:40:41 -0600

    cambios

--- RestaurantePW/Areas/Administrador/Controllers/PlatillosController.cs
+++ RestaurantePW/Areas/Administrador/Controllers/PlatillosController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantePW.Areas.Administrador.Models;
+using RestaurantePW.Models;
+using System.Data;
 
 namespace RestaurantePW.Areas.Administrador.Controllers
 {
+    [Authorize(Roles = "Administrador, Supervisor")]
+    [Area("Administrador")]
     public class PlatillosController : Controller
     {
-        public IActionResult Index()
+        private readonly supertacoContext context;
+        private readonly IWebHostEnvironment em;
+        public PlatillosController(supertacoContext context, IWebHostEnvironment em)
         {
-            return View();
+            this.context = context;
+            this.em = em;
+        }
+        public IActionResult Index(IndexPlatillosVM vm)
+        {
+            if (vm.IdCategoria == 0)//Esto sera en caso de que no hayan seleccionado nada en el combobox
+                vm.Platillos = context.Platillos.Include(x => x.IdCategoriaNavigation).OrderBy(x => x.Nombre);
+            else
+                vm.Platillos = context.Platillos.Include(x => x.IdCategoriaNavigation).Where(x => x.IdCategoria == vm.IdCategoria).OrderBy(x => x.Nombre);
+            vm.Categorias = context.Categorias.OrderBy(x => x.Nombre);
+            return View(vm);
         }
     }
 }
--- RestaurantePW/Areas/Administrador/Models/IndexPlatillosVM.cs
+++ RestaurantePW/Areas/Administrador/Models/IndexPlatillosVM.cs
@@ -1,6 +1,12 @@
-﻿namespace RestaurantePW.Areas.Administrador.Models
+﻿using RestaurantePW.Models;
+
+namespace RestaurantePW.Areas.Administrador.Models
 {
     public class IndexPlatillosVM
     {
+
+        public IEnumerable<Categoria>? Categorias { get; set; }
+        public IEnumerable<Platillo>? Platillos { get; set; }
+        public int IdCategoria { get; set; }
     }
 }
